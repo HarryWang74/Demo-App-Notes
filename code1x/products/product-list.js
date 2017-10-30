4e50a028ecef8a6cd30b53fef08497b0a163ac6b
Harry <harry74323@gmail.com>
2017-10-31 10:46:40 +1100

    1.5 step3 done

---
+++ code1x/products/product-list.js
@@ -0,0 +1,11 @@
+angular.module('myApp').component('productsList', {
+    templateUrl: "products/product-list.html",
+    controllerAs: "ctrl",
+    controller: [function(){
+        var ctrl = this;
+
+        ctrl.$onInit = function () {
+
+        };
+    }],
+});
