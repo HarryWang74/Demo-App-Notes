034c6c915c99117962fedbbca6935352c9b201d0
Harry <harry74323@gmail.com>
2017-11-01 15:41:14 +1100

    angular1.5 step6

--- code1x/products/product-list.js
+++ code1x/products/product-list.js
@@ -3,7 +3,24 @@ angular.module('myApp').component('productsList', {
     controllerAs: "ctrl",
     controller: [function(){
         var ctrl = this;
-
+        ctrl.products = [
+            {
+                "productId": 1,
+                "productName": "ProductName01",
+                "price": 19.95,
+                "rating": 1,
+                "imageUrl": "http://via.placeholder.com/2000x600",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            },
+            {
+                "productId": 2,
+                "productName": "ProductName02",
+                "price": 29.95,
+                "rating": 2,
+                "imageUrl": "http://via.placeholder.com/2000x600",
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+            },
+        ]
         ctrl.$onInit = function () {
 
         };
