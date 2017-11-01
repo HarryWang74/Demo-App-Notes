12f9a5a8e0badf2ef967248dd12ac184979af8d7
Harry <harry74323@gmail.com>
2017-11-01 16:52:45 +1100

    angular4 step6

---
+++ code4/src/app/products/i-product.ts
@@ -0,0 +1,8 @@
+export interface IProduct {
+    "productId": number;
+    "productName": string;
+    "price": number;
+    "rating": number;
+    "imageUrl": string;
+    "description": string;
+}
