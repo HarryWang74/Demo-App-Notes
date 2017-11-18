0ee66a628048d29fcaa02686fdc5fc0fd8c3b72e
harry <harry74323@gmail.com>
2017-11-19 10:31:30 +1100

    angular1.5 steps 11

--- code1x/products/rating.js
+++ code1x/products/rating.js
@@ -1,10 +1,30 @@
 angular.module('myApp').component('rating', {
     templateUrl: "/products/rating.html",
     controllerAs: "ctrl",
+    bindings:{
+      rating: '<'
+    },
     controller: [function(){
         var ctrl = this;
-  
+
+        ctrl.maxRating = 5;
+
         ctrl.$onInit = function () {
         };
+
+        ctrl.getMaxRating = function(){
+            return new Array(ctrl.maxRating);
+        }
+
+        ctrl.changeRating = function(index){
+            ctrl.rating = index + 1;
+        }
+
+        ctrl.$onChanges = function (changes) {
+            console.log(changes);
+            if (typeof (changes.rating) != "undefined" && typeof (changes.rating.currentValue) != "undefined") {
+                ctrl.rating = changes.rating.currentValue;
+            }
+        }
     }],
   });
