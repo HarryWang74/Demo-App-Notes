4ff3fa0b401f9564b651bb123d00cf9b2d226e81
harry <harry74323@gmail.com>
2017-11-19 11:37:55 +1100

    angular4 step11

--- code4/src/app/products/rating/rating.component.ts
+++ code4/src/app/products/rating/rating.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-rating',
@@ -8,8 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class RatingComponent implements OnInit {
 
   constructor() { }
-
+  maxRating:number = 5;
+  @Input() rate: number;
   ngOnInit() {
   }
 
+  getMaxRating = function(){
+    return new Array(this.maxRating);
+  }
+
+  changeRate = function(index){
+    this.rate = index + 1;
+  }
 }
